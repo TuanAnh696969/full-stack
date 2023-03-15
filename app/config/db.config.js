81125d6956ee22ed055160a7790b7632633092d6
@TuanAnh00069 <tuananh567780@gmail.com>
2023-03-15 08:17:14 +0700

    first commit

---
+++ app/config/db.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  HOST: "165.232.172.0",
+  USER: "root",
+  PASSWORD: "tiger",
+  DB: "usersss",
+  dialect: "postgresql",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+};
+
+
+
